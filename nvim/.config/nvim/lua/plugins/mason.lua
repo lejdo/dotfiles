5a710411a9df692a34c3ce3154081c5c07bc1ef2
= <=>
2025-12-23 22:30:23 +0100

    feat/fix window controlls and mason fix

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,20 +1,19 @@
 return {
-  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+    },
     lazy = false,
     priority = 1000,
     config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "pyright",
-          "ts_ls",
-          "clangd",
-          "lua_ls",
-        },
-      })
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "pyright",
+                "ts_ls",
+                "clangd",
+                "lua_ls",
+            },
+        })
     end,
-  },
 }
-
