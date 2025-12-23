e937930ac164e8d88b12f0677bd6a12d297a5fbc
= <=>
2025-12-23 15:56:44 +0100

    feat: auto readme script

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dotfiles
+
+My personal dotfiles for Arch Linux + Hyprland.
+
+---
+
+<!-- AUTO:START -->
+## Managed programs (GNU Stow)
+
+- hypr
+- scrips
+- waybar
+- wofi
+
+<!-- AUTO:END -->
+
+---
+
+## Notes
+- Window manager: Hyprland
+- Package manager: pacman + yay
+- Dotfiles managed using GNU Stow
+
---
+++ scrips/generate-readme.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+README="README.md"
+START="<!-- AUTO:START -->"
+END="<!-- AUTO:END -->"
+
+# Collect stow paackages
+PACKAGES=$(find . -maxdepth 1 -type d \
+  ! -name "." \
+  ! -name ".git" \
+  ! -name "scripts" \
+  -printf "%f\n" | sort)
+
+AUTO_CONTENT="## Managed programs (GNU Stow)\n\n"
+for pkg in $PACKAGES; do
+  AUTO_CONTENT+="- $pkg\n"
+done
+
+# Replace auto section
+awk -v start="$START" -v end="$END" -v content="$AUTO_CONTENT" '
+  $0 == start { print; print content; skip=1; next }
+  $0 == end   { skip=0 }
+  !skip
+' "$README" > "$README.tmp"
+
+mv "$README.tmp" "$README"
