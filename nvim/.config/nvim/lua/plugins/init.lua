8f16b6604601f8f2c99f4fe0b22441b4fafefd9a
= <=>
2025-12-23 19:44:32 +0100

    fix: fix

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,16 +1,9 @@
-
--- lua/plugins/init.lua
-local plugins = {}
-
--- add mason first
-table.insert(plugins, require("plugins.mason"))
-
--- add the rest
-table.insert(plugins, require("plugins.lsp"))
-table.insert(plugins, require("plugins.treesitter"))
-table.insert(plugins, require("plugins.telescope"))
-table.insert(plugins, require("plugins.completion"))
-table.insert(plugins, require("plugins.colorscheme"))
-
-return plugins
+return {
+    require("plugins.mason"),
+    require("plugins.lsp"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.completion"),
+    require("plugins.colorscheme"),
+}
 
