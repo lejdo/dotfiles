017755048abe02f9fa0d93243ebc2e1bafef24ff
Oscar Angantyr <oscar.angantyr@gmail.com>
2026-09-20 03:42:59 +0200

    feat: bootstrap setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+set -e
+
 cd "$(dirname "$0")"
+
+MACHINE="$1"
+
+if [[ "$MACHINE" != "desktop" && "$MACHINE" != "laptop" ]]; then
+    echo "Usage: $0 {desktop|laptop}"
+    exit 1
+fi
+
+# maybe add script to install packages
+#
+# Maybe make script for stow alone more modular ish
 
 stow zsh
 stow kitty
@@ -6,3 +21,7 @@ stow nvim
 stow hypr
 
 sudo install -Dm644 system/modprobe/alsa.conf /etc/modprobe.d/alsa.conf
+
+./scripts/machine.sh "$MACHINE"
+
+echo "Bootstrap complete for: $MACHINE"
